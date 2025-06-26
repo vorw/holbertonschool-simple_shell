9f99f6c61af653f027b90ec858fdcb95a65a0322
vorw <sultanalsanie23@gmail.com>
2025-06-26 14:01:45 +0000

    added custom file for task 1

---
+++ betty-code.c
@@ -0,0 +1,8 @@
+int main(void)
+{
+	char *name = "Betty";
+
+	printf("%s isn't just a style, Betty Holberton, is a pioneering computer scientist and one of the original programmers of the ENIAC computer.", name);
+
+	return (0);
+}
