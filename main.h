e260d44478822843c230e56a56b50c472ddfc783
moha88z <mohammed.alayda@gmail.com>
2025-07-02 12:19:58 +0000

    task2

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+extern char **environ;
+
+void simple_shell(void);
+
+#endif
