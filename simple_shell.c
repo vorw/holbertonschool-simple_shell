854a4ecbf7eca5d09556963e9b4be11ae28d7bd7
vorw <sultanalsanie23@gmail.com>
2025-07-02 10:14:35 +0000

    Task 2

---
+++ simple_shell.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/**
+ * main - Entry point
+ * Description: simple shell
+ * Return: 0
+ */
+
+extern char **environ;
+
+int main(void)
+{
+	char *line = NULL;
+	size_t size = 0;
+	ssize_t characters;
+	pid_t pid;
+
+	while (1)
+	{
+		printf("$ ");
+		characters = getline(&line, &size, stdin);
+		if (characters == -1)
+		{
+			printf("\n");
+			break;
+		}
+		if (line[characters - 1] == '\n')
+			line[characters - 1] = '\0';
+		pid = fork();
+
+		if (pid == 0)
+		{
+			char *args[] = {line, NULL};
+
+			if (execve(line, args, environ) == -1)
+			{
+				perror("Error");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else if (pid > 0)
+		{
+			wait(NULL);
+		}
+		else
+		{
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
+	}
+	free(line);
+	return (0);
+}
