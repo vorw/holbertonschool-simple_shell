e0c7d7486b8558664ced63952d597d744e820f73
vorw <sultanalsanie23@gmail.com>
2025-07-04 14:16:05 +0000

    fixing path

--- executer.c
+++ executer.c
@@ -12,17 +12,14 @@ void execute_command(char **args, char *program_name)
 	struct stat st;
 
 	if (stat(args[0], &st) == 0)
-	{
 		full_path = args[0];
-	}
 	else
-	{
 		full_path = getenv_path(args[0]);
-		if (!full_path)
-		{
-			fprintf(stderr, "%s: 1: %s: not found\n", program_name, args[0]);
-			return;
-		}
+
+	if (!full_path)
+	{
+		fprintf(stderr, "%s: 1: %s: not found\n", program_name, args[0]);
+		exit(127);
 	}
 
 	pid = fork();
@@ -33,39 +30,35 @@ void execute_command(char **args, char *program_name)
 		exit(EXIT_FAILURE);
 	}
 	else if (pid > 0)
-	{
 		wait(NULL);
-	}
 
 	if (full_path != args[0])
 		free(full_path);
 }
 
 /**
- * getenv_path - Finds the full path of a command in PATH (manually).
+ * getenv_path - Finds the full path of a command in PATH.
  * @command: Command name.
  *
  * Return: Full path or NULL.
  */
 char *getenv_path(char *command)
 {
-	int i = 0;
-	char *path_env = NULL;
-	char *path_copy = NULL;
-	char *dir = NULL;
-	char *full_path = NULL;
+	char *path_env = NULL, *path_copy = NULL, *dir = NULL, *full_path = NULL;
 	struct stat st;
+	int i = 0;
 
-	for (i = 0; environ[i]; i++)
+	while (environ[i])
 	{
 		if (strncmp(environ[i], "PATH=", 5) == 0)
 		{
 			path_env = environ[i] + 5;
 			break;
 		}
+		i++;
 	}
 
-	if (!path_env)
+	if (!path_env || path_env[0] == '\0')
 		return (NULL);
 
 	path_copy = strdup(path_env);
@@ -73,7 +66,7 @@ char *getenv_path(char *command)
 		return (NULL);
 
 	dir = strtok(path_copy, ":");
-	while (dir)
+	while (dir != NULL)
 	{
 		full_path = malloc(strlen(dir) + strlen(command) + 2);
 		if (!full_path)
@@ -88,6 +81,7 @@ char *getenv_path(char *command)
 			free(path_copy);
 			return (full_path);
 		}
+
 		free(full_path);
 		dir = strtok(NULL, ":");
 	}
